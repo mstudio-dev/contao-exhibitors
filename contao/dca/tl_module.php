8a0299f049af55647c19292fba4116c917cc0539
Markus Schnagl <mail@mstudio.de>
2026-03-04 20:27:00 +0100

    Add imgSize selector to module settings for logo resizing

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 // Template-Auswahl für das Ausstellerlisten-Modul einblenden
 $GLOBALS['TL_DCA']['tl_module']['palettes']['exhibitor_list'] =
     '{title_legend},name,headline,type;'
+    . '{image_legend:hide},imgSize;'
     . '{template_legend:hide},customTpl;'
     . '{protected_legend:hide},protected;'
     . '{expert_legend:hide},guests,cssID';
--- src/Controller/FrontendModule/ExhibitorListController.php
+++ src/Controller/FrontendModule/ExhibitorListController.php
@@ -6,10 +6,13 @@ namespace Mstudio\ContaoExhibitorsBundle\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
+use Contao\CoreBundle\Image\ImageFactoryInterface;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\FilesModel;
 use Contao\ModuleModel;
+use Contao\StringUtil;
 use Mstudio\ContaoExhibitorsBundle\Model\ExhibitorModel;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -20,26 +23,51 @@ use Symfony\Component\HttpFoundation\Response;
 )]
 class ExhibitorListController extends AbstractFrontendModuleController
 {
+    public function __construct(
+        private readonly ImageFactoryInterface $imageFactory,
+        #[Autowire('%kernel.project_dir%')]
+        private readonly string $projectDir,
+    ) {
+    }
+
     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
     {
         $exhibitors = ExhibitorModel::findAllPublished();
+        $size = StringUtil::deserialize($model->imgSize);
 
         $rows = [];
 
         if (null !== $exhibitors) {
             foreach ($exhibitors as $exhibitor) {
-                $logoModel = null;
+                $logoPath = null;
 
                 if ($exhibitor->logo) {
                     $logoModel = FilesModel::findByUuid($exhibitor->logo);
+
+                    if (null !== $logoModel) {
+                        if (!empty($size[0]) || !empty($size[1]) || !empty($size[2])) {
+                            try {
+                                $imageObj = $this->imageFactory->create(
+                                    $this->projectDir . '/' . $logoModel->path,
+                                    $size,
+                                );
+                                $logoPath = $imageObj->getUrl($this->projectDir);
+                            } catch (\Exception) {
+                                $logoPath = $logoModel->path;
+                            }
+                        } else {
+                            $logoPath = $logoModel->path;
+                        }
+                    }
                 }
 
                 $rows[] = [
-                    'firmenname'  => $exhibitor->firmenname,
-                    'standplatz'  => $exhibitor->standplatz,
-                    'reserviert'  => (bool) $exhibitor->reserviert,
-                    'website'     => $exhibitor->website,
-                    'logo'        => $logoModel,
+                    'firmenname' => $exhibitor->firmenname,
+                    'standplatz' => $exhibitor->standplatz,
+                    'reserviert' => (bool) $exhibitor->reserviert,
+                    'website'    => $exhibitor->website,
+                    'logoPath'   => $logoPath,
+                    'logoAlt'    => $exhibitor->firmenname,
                 ];
             }
         }
