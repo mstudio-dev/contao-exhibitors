b05290d4975535fab6729f010c9dec63066fab6c
Markus Schnagl <mail@mstudio.de>
2026-09-15 21:34:38 +0200

    Register template path explicitly via compiler pass

--- src/ContaoExhibitorsBundle.php
+++ src/ContaoExhibitorsBundle.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Mstudio\ContaoExhibitorsBundle;
 
+use Mstudio\ContaoExhibitorsBundle\DependencyInjection\Compiler\AddTemplatePathPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ContaoExhibitorsBundle extends Bundle
@@ -12,4 +14,11 @@ class ContaoExhibitorsBundle extends Bundle
     {
         return \dirname(__DIR__);
     }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new AddTemplatePathPass());
+    }
 }
+
