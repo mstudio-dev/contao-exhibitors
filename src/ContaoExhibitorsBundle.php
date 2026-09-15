1e7071b4d5027e0f761c97d7696b689145a5bdc4
Markus Schnagl <mail@mstudio.de>
2026-09-15 21:49:18 +0200

    Revert broken compiler pass, register templates via Twig prepend

--- src/ContaoExhibitorsBundle.php
+++ src/ContaoExhibitorsBundle.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Mstudio\ContaoExhibitorsBundle;
 
-use Mstudio\ContaoExhibitorsBundle\DependencyInjection\Compiler\AddTemplatePathPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ContaoExhibitorsBundle extends Bundle
@@ -13,12 +11,6 @@ class ContaoExhibitorsBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
-    }
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-        $container->addCompilerPass(new AddTemplatePathPass());
     }
 }
 
--- src/DependencyInjection/ContaoExhibitorsExtension.php
+++ src/DependencyInjection/ContaoExhibitorsExtension.php
@@ -7,10 +7,20 @@ namespace Mstudio\ContaoExhibitorsBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoExhibitorsExtension extends Extension
+class ContaoExhibitorsExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        // Ensure templates are discoverable by Twig regardless of Contao's
+        // filesystem loader cache state.
+        $container->prependExtensionConfig('twig', [
+            'paths' => [\dirname(__DIR__, 2) . '/contao/templates' => null],
+        ]);
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
